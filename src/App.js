f2db0697a59cf74b05a666d9ee102ebb383880ef
Alex Marriott <marriottaog@gmail.com>
2021-09-29 09:53:33 +0100

    initial app basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PubNub from 'pubnub';
+import { PubNubProvider, usePubNub } from 'pubnub-react';
+
+import Login from './components/Login';
+
+const pubnub = new PubNub({
+  publishKey: 'pub-c-854fa4a9-e5f4-42f0-9bf4-b060ac55fd65',
+  subscribeKey: 'sub-c-6fb4329a-2042-11ec-8d5d-a65b09ab59bc'
+  // uuid: 'myUniqueUUID'
+});
+
+// how do init pubnub for diff people with diff UUID, surely same pub/sub keys?
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <PubNubProvider client={pubnub}>
+    //   <Chat />
+    // </PubNubProvider>
+    <Login></Login>
+  );
+}
+
+function Chat() {
+  const pubnub = usePubNub();
+  const [channels] = useState(['awesome-channel']);
+  const [messages, addMessage] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleMessage = event => {
+    const message = event.message;
+    if (typeof message === 'string' || message.hasOwnProperty('text')) {
+      const text = message.text || message;
+      addMessage(messages => [...messages, text]);
+    }
+  };
+
+  const sendMessage = message => {
+    if (message) {
+      pubnub
+        .publish({ channel: channels[0], message })
+        .then(() => setMessage(''));
+    }
+  };
+
+  useEffect(() => {
+    pubnub.addListener({ message: handleMessage });
+    pubnub.subscribe({ channels });
+  }, [pubnub, channels]);
+
+  return (
+    <div style={pageStyles}>
+      <div style={chatStyles}>
+        <div style={headerStyles}>React Chat Example</div>
+        <div style={listStyles}>
+          {messages.map((message, index) => {
+            return (
+              <div key={`message-${index}`} style={messageStyles}>
+                {message}
+              </div>
+            );
+          })}
+        </div>
+        <div style={footerStyles}>
+          <input
+            type="text"
+            style={inputStyles}
+            placeholder="Type your message"
+            value={message}
+            onKeyPress={e => {
+              if (e.key !== 'Enter') return;
+              sendMessage(message);
+            }}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button
+            style={buttonStyles}
+            onClick={e => {
+              e.preventDefault();
+              sendMessage(message);
+            }}
+          >
+            Send Message
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
+
+const pageStyles = {
+  alignItems: 'center',
+  background: '#282c34',
+  display: 'flex',
+  justifyContent: 'center',
+  minHeight: '100vh',
+};
+
+const chatStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '50vh',
+  width: '50%',
+};
+
+const headerStyles = {
+  background: '#323742',
+  color: 'white',
+  fontSize: '1.4rem',
+  padding: '10px 15px',
+};
+
+const listStyles = {
+  alignItems: 'flex-start',
+  backgroundColor: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+  overflow: 'auto',
+  padding: '10px',
+};
+
+const messageStyles = {
+  backgroundColor: '#eee',
+  borderRadius: '5px',
+  color: '#333',
+  fontSize: '1.1rem',
+  margin: '5px',
+  padding: '8px 15px',
+};
+
+const footerStyles = {
+  display: 'flex',
+};
+
+const inputStyles = {
+  flexGrow: 1,
+  fontSize: '1.1rem',
+  padding: '10px 15px',
+};
+
+const buttonStyles = {
+  fontSize: '1.1rem',
+  padding: '10px 15px',
+};
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  return (
+    <p>hey</p>
+  )
+}
+
+export default Login
