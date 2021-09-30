8f92aa2f09498a23eda5d9069bf784c0e7f0fc44
Alex Marriott <marriottaog@gmail.com>
2021-09-30 14:52:48 +0100

    can add/delete channels

--- src/components/pubnub/Chat.js
+++ src/components/pubnub/Chat.js
@@ -9,36 +9,33 @@ function Chat(props) {
   const [currentChannel, setCurrentChannel] = useState('');
   const [messages, addMessage] = useState({});
   const [message, setMessage] = useState('');
-  const [msg, newmsg] = useState({});
+  const [msg, newmsg] = useState(['', {}]);
   const [listenerAdded, setListenerAdded] = useState(false);
 
-  const getMessages = () => {
-    addMessage([])
-    pubnub.fetchMessages(
-        {
-            channels: channels,
-            end: '15343325004275466',
-            count: 100
-        },
-        (status, response) => {
-          const obj = {}
-          channels.forEach(ch => {
-            obj[ch] = []
-            const channelAccessString = ch.replace(' ', '%20')
-            response.channels[channelAccessString]?.forEach(msg => {
-              obj[ch].push({message: msg.message, user: msg.uuid})
-            })
-          })
-          addMessage(obj)
-        }
-    );
-  }
+  useEffect(() => {
+    getMyChannels();
+    getAllChannels();
+  }, []);
 
   useEffect(() => {
     if (channels.length > 0) {
       getMessages();
     }
   }, [channels]);
+
+  useEffect(() => {
+    pubnub.subscribe({ channels });
+    if (!listenerAdded) {
+      pubnub.addListener({ message: handleMessage });
+      setListenerAdded(true)
+    }
+  }, [pubnub, channels]);
+
+  useEffect(() => {
+    if (Object.keys(msg[1]).length > 0) {
+      doThing();
+    }
+  }, [msg]);
 
   const getMyChannels = () => {
     fetch(`http://localhost:3000/api/v1/channels/1?username=${props.uuid}`, {
@@ -65,22 +62,61 @@ function Chat(props) {
     })
     .then((response) => response.json())
     .then((data) =>  {
-      data.forEach(channel => setAllChannels(channels => [...channels, channel.name]))
+      let channelArray = []
+      data.forEach(channel => channelArray.push(channel.name))
+      setAllChannels(channelArray)
     })
   }
 
-  useEffect(() => {
-    getMyChannels();
-    getAllChannels();
-  }, []);
+  const joinChannel = () => {
+    fetch('http://localhost:3000/api/v1/user_channels', {
+      method: 'post',
+      body: JSON.stringify({user_id: [props.uuid], channel_id: [currentChannel]}),
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+    })
+    .then(() => setChannels(channels => [...channels, currentChannel]))
+  }
 
-  useEffect(() => {
-    pubnub.subscribe({ channels });
-    if (!listenerAdded) {
-      pubnub.addListener({ message: handleMessage });
-      setListenerAdded(true)
-    }
-  }, [pubnub, channels]);
+  const leaveChannel = () => {
+    fetch('http://localhost:3000/api/v1/user_channels/1', {
+      method: 'delete',
+      body: JSON.stringify({user_id: [props.uuid], channel_id: [currentChannel]}),
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      const channelIndex = channels.indexOf(currentChannel)
+      setChannels(ch => channels.splice(channelIndex, 1))
+    })
+  }
+
+  const getMessages = () => {
+    addMessage([])
+    pubnub.fetchMessages(
+        {
+            channels: channels,
+            end: '15343325004275466',
+            count: 100
+        },
+        (status, response) => {
+          const obj = {}
+          channels.forEach(ch => {
+            obj[ch] = []
+            const channelAccessString = ch.replace(' ', '%20')
+            response.channels[channelAccessString]?.forEach(msg => {
+              obj[ch].push({message: msg.message, user: msg.uuid})
+            })
+          })
+          addMessage(obj)
+        }
+    );
+  }
 
   const handleMessage = event => {
     const channel = event.channel
@@ -88,18 +124,12 @@ function Chat(props) {
     const message = event.message;
     if (typeof message === 'string' || message.hasOwnProperty('text')) {
       const text = message.text || message;
-      newmsg({message: text, user: publisher})
+      newmsg([channel, {message: text, user: publisher}])
     }
   };
 
-  useEffect(() => {
-    if (Object.keys(msg).length > 0) {
-      doThing();
-    }
-  }, [msg]);
-
   const doThing = () => {
-    addMessage({...messages, [currentChannel]: [...messages[currentChannel], msg]})
+    addMessage({...messages, [msg[0]]: [...messages[msg[0]], msg[1]]})
   }
 
   const sendMessage = message => {
@@ -110,6 +140,54 @@ function Chat(props) {
     }
   };
 
+  let chatScreen
+
+  if (channels.includes(currentChannel)) {
+    chatScreen = <RightScreen joinChannel={false}>
+                   <ChatContainer>
+                     {messages[currentChannel]?.map(msg => msg).reverse().map((message, index) => {
+                       return (
+                         <Message key={`message-${index}`}>
+                           {message.user} - {message.message}
+                         </Message>
+                        );
+                      })}
+                     <a
+                       onClick={e => {
+                         e.preventDefault();
+                         leaveChannel();
+                       }}
+                     >
+                       <DeleteButton src="delete-button.svg" alt=""/>
+                     </a>
+                   </ChatContainer>
+                   <InputContainer>
+                     <Input
+                       type="text"
+                       placeholder="Type your message"
+                       value={message}
+                       onKeyPress={e => {
+                         if (e.key !== 'Enter') return;
+                         sendMessage(message);
+                       }}
+                       onChange={e => setMessage(e.target.value)}
+                     />
+                     <a
+                       onClick={e => {
+                         e.preventDefault();
+                         sendMessage(message);
+                       }}
+                     >
+                       <SendButton src="send.png" alt=""/>
+                     </a>
+                   </InputContainer>
+                 </RightScreen>
+  } else {
+    chatScreen = <RightScreen joinChannel={true}>
+                   <JoinChannel onClick={() => joinChannel()}>Join Channel?</JoinChannel>
+                 </RightScreen>
+  }
+
   return (
     <Container>
       <LeftScreen>
@@ -118,56 +196,31 @@ function Chat(props) {
           <Username>{props.uuid}</Username>
         </ProfileContainer>
         <ChannelsContainer>
+          <p>My channels</p>
           {channels?.map(channel => {
+            return <ChannelButton onClick={() => setCurrentChannel(channel)}>{channel}</ChannelButton>
+          })}
+          <p>Other channels</p>
+          {allChannels.filter(el => !channels.includes(el))?.map(channel => {
             return <ChannelButton onClick={() => setCurrentChannel(channel)}>{channel}</ChannelButton>
           })}
         </ChannelsContainer>
       </LeftScreen>
-      <RightScreen>
-        <ChatContainer>
-          {messages[currentChannel]?.map(msg => msg).reverse().map((message, index) => {
-            return (
-              <Message key={`message-${index}`}>
-                {message.user} - {message.message}
-              </Message>
-            );
-          })}
-        </ChatContainer>
-        <InputContainer>
-          <Input
-            type="text"
-            placeholder="Type your message"
-            value={message}
-            onKeyPress={e => {
-              if (e.key !== 'Enter') return;
-              sendMessage(message);
-            }}
-            onChange={e => setMessage(e.target.value)}
-          />
-          <a
-            onClick={e => {
-              e.preventDefault();
-              sendMessage(message);
-            }}
-          >
-            <SendButton src="send.png" alt=""/>
-          </a>
-        </InputContainer>
-      </RightScreen>
+      {chatScreen}
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
-  padding: 25px;
-  height: 100%;
-  // overflow: hidden;
+  height: 100vh;
   background-color: #150050;
   color: white;
 `;
 
 const LeftScreen = styled.div`
+  display: flex;
+  flex-direction: column;
   flex-basis: 25%;
   background-color: #3D2C8D;
   border-radius: 10px 0 0 10px;
@@ -199,6 +252,17 @@ const ChannelsContainer = styled.div`
   flex-direction: column;
   margin-top: 16px;
   margin-left: 8px;
+  height: 80vh;
+`;
+
+const DeleteButton = styled.img`
+  position: absolute;
+  height: 30px;
+  right: 0px;
+  top: 0px;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const ChannelButton = styled.button`
@@ -219,11 +283,15 @@ const ChannelButton = styled.button`
 `;
 
 const RightScreen = styled.div`
-  padding-left: 24px;
+  padding: 24px;
   flex-basis: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: ${props => (props.joinChannel ? 'center' : 'space-between')};
 `;
 
 const ChatContainer = styled.div`
+  position: relative;
   display: flex;
   align-items: flex-start;
   flex-direction: column-reverse;
@@ -249,6 +317,7 @@ const InputContainer = styled.div`
 const Input = styled.input`
   width: 100%;
   height: 24px;
+  padding: 8px;
   border-radius: 6px 0 0 6px;
 `;
 
@@ -256,6 +325,21 @@ const SendButton = styled.img`
   height: 30px;
   border-radius: 0 6px 6px 0;
   &:hover {
+    cursor: pointer;
+  }
+`;
+
+const JoinChannel = styled.button`
+  color: white;
+  height: 64px;
+  border-radius: 16px;
+  border: none;
+  font-weight: bold;
+  background-color: #3D2C8D;
+  font-size: 14px;
+  transition: background 0.3s ease;
+  &:hover {
+    background: #3B889B;
     cursor: pointer;
   }
 `;
